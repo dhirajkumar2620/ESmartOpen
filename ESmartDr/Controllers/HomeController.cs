111cd23883f63e61df75f7930c8d69a1ae9a4aea
dhirajkumar2620 <shejaledhirajkumar@gmail.com>
2020-06-06 17:03:35 +0530

    Add Project

---
+++ ESmartDr/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EsmartDr.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("LogIn");
+           
+        }
+
+        public ActionResult Registration()
+        {
+            return View("Registration");
+        }
+
+         public ActionResult StaffRegistration()
+        {
+            return View("StaffRegistration");
+        }
+        public ActionResult PatientRegistration()
+        {
+            return View("PatientRegistration");
+        }
+
+        public ActionResult MyOPD()
+        {
+            return View("MyOPD");
+        }
+        public ActionResult AddMedicine()
+        {
+            return View("AddMedicine");
+        }
+        public ActionResult CampaignMaster()
+        {
+            return View("CampaignMaster");
+        }
+        public ActionResult Apoinment()
+        {
+            return View("Apoinment");
+        }
+        public ActionResult AllPatient()
+        {
+            return View("AllPatient");
+        }
+        public ActionResult AdminManagement()
+        {
+            return View("AdminManagement"); 
+        }
+        public ActionResult ReceptionManagement()
+        {
+            return View("ReceptionManagement"); 
+        }
+        public ActionResult Lab1()
+        {
+            return View("Lab1");
+        }
+        public ActionResult Lab2()
+        {
+            return View("Lab2");
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ESmartDr/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ESmartDr.Startup))]
+namespace ESmartDr
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
