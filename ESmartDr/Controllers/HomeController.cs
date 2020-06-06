ad43cbcb3776e8c13a8212d158ec382d82d0f822
kiranlasure95@gmail.com <kiranlasure95@gmail.com>
2020-06-06 20:01:44 +0530

    by shital 8:01

--- ESmartDr/Controllers/HomeController.cs
+++ ESmartDr/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace EsmartDr.Controllers
 {
     public class HomeController : Controller
     {
+        //Check in by shital
         public ActionResult Index()  
         {
             return View("LogIn");
