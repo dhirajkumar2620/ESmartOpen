d8f8d883175a40a62fbb698ef090c36a8af29676
dhirajkumar2620 <shejaledhirajkumar@gmail.com>
2020-06-20 20:14:05 +0530

    ui changs

---
+++ ESmartDr/Controllers/AccountDetailsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ESmartDr.Controllers
+{
+    public class AccountDetailsController : Controller
+    {
+        // GET: AccountDetails
+        public ActionResult Index()
+        {
+            return View("AccountDetails");
+        }
+    }
+}
