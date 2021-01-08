d28399509a4a31cedfd6c6029eea4a9ffcb4f183
dhirajkumar2620 <shejaledhirajkumar@gmail.com>
2021-01-08 23:05:28 +0530

    Add Dal @ Bal layer for dentalpage

---
+++ Dal_Layer/Dal_DentalDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal_Layer
+{
+    public class Dal_DentalDetails
+    {
+        // Use Dal_medicineDetails
+    }
+}
