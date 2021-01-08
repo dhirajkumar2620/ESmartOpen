b17f5e23eec234a4187801e942b1f73238707a28
dhirajkumar2620 <shejaledhirajkumar@gmail.com>
2021-01-08 22:16:34 +0530

    Add adult and pediatric class

---
+++ App_Layer/AdultDetails.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Layer
+{
+    public class AdultDetails
+    {
+        public int Id { get; set; }
+        public int T1 { get; set; }
+        public int T2 { get; set; }
+        public int T3 { get; set; }
+        public int T4 { get; set; }
+        public int T5 { get; set; }
+        public int T6 { get; set; }
+        public int T7 { get; set; }
+        public int T8 { get; set; }
+        public int T9 { get; set; }
+        public int T10 { get; set; }
+        public int T11 { get; set; }
+        public int T12 { get; set; }
+        public int T13 { get; set; }
+        public int T14 { get; set; }
+        public int T15 { get; set; }
+        public int T16 { get; set; }
+        public int T17 { get; set; }
+        public int T18 { get; set; }
+        public int T19 { get; set; }
+        public int T20 { get; set; }
+        public int T21 { get; set; }
+        public int T22 { get; set; }
+        public int T23 { get; set; }
+        public int T24 { get; set; }
+        public int T25 { get; set; }
+        public int T26 { get; set; }
+        public int T27 { get; set; }
+        public int T28 { get; set; }
+        public int T29 { get; set; }
+        public int T30 { get; set; }
+        public int T31 { get; set; }
+        public int T32 { get; set; }
+        public int ColorCode { get; set; }
+        public string ToothProcedure { get; set; }
+        public string Amount { get; set; }
+        public string Notes { get; set; }
+        public string CasePaperNo { get; set; }
+        public string HospitalId { get; set; }
+        public int PatientId { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifideDate { get; set; }
+        public int ModifideBy { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ App_Layer/PediatricDetails.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Layer
+{
+    public class PediatricDetails
+    {
+        public int Id { get; set; }
+        public int A { get; set; }
+        public int B { get; set; }
+        public int C { get; set; }
+        public int D { get; set; }
+        public int E { get; set; }
+        public int F { get; set; }
+        public int G { get; set; }
+        public int H { get; set; }
+        public int I { get; set; }
+        public int J { get; set; }
+        public int K { get; set; }
+        public int L { get; set; }
+        public int M { get; set; }
+        public int N { get; set; }
+        public int O { get; set; }
+        public int P { get; set; }
+        public int Q { get; set; }
+        public int R { get; set; }
+        public int S { get; set; }
+        public int T { get; set; }
+        public int ColorCode { get; set; }
+        public string ToothProcedure { get; set; }
+        public string Amount { get; set; }
+        public string Notes { get; set; }
+        public string CasePaperNo { get; set; }
+        public string HospitalId { get; set; }
+        public int PatientId { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifideDate { get; set; }
+        public int ModifideBy { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
